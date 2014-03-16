4da8dfe83cbf555554eaeb9f063cb8bd7ba4d971
alexzicat <dev.alexzicat@gmail.com>
2014-03-16 00:39:46 -0400

    Add rspec testing to gem (missing files VCR)

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'tongues'
+require 'vcr'
+require 'webmock'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.default_cassette_options = {:record => :new_episodes}
+end
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  config.order = 'random'
+end
