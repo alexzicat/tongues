f2b400eff4a69f68c0b88918bb6a916e70845a5d
alexzicat <dev.alexzicat@gmail.com>
2014-03-15 22:36:14 -0400

    generator correction

--- lib/generators/tongues/initializer_generator.rb
+++ lib/generators/tongues/initializer_generator.rb
@@ -1,11 +1,12 @@
 require 'rails/generators'
 
-class InitializerGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
+module Tongues
+  class InitializerGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
 
-  desc 'This generator creates an initializer for the api key.'
-
-  def copy_initializer_file
-    copy_file 'initializer.rb', 'config/initializers/tongues.rb'
+    desc 'This generator creates an initializer for the api key.'
+    def copy_initializer_file
+      copy_file 'initializer.rb', 'config/initializers/tongues.rb'
+    end
   end
 end
--- lib/tongues/version.rb
+++ lib/tongues/version.rb
@@ -1,3 +1,3 @@
 module Tongues
-  VERSION = "0.0.9"
+  VERSION = "0.0.10"
 end
