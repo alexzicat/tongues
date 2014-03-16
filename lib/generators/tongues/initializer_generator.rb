e417351d62be2f9752c44741005661a94e61ad74
alexzicat <dev.alexzicat@gmail.com>
2014-03-15 22:33:20 -0400

    generator correction

---
+++ lib/generators/tongues/initializer_generator.rb
@@ -0,0 +1,11 @@
+require 'rails/generators'
+
+class InitializerGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  desc 'This generator creates an initializer for the api key.'
+
+  def copy_initializer_file
+    copy_file 'initializer.rb', 'config/initializers/tongues.rb'
+  end
+end
--- lib/tongues/version.rb
+++ lib/tongues/version.rb
@@ -1,3 +1,3 @@
 module Tongues
-  VERSION = "0.0.8"
+  VERSION = "0.0.9"
 end
